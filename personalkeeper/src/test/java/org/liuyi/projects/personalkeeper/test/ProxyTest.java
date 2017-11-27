7d778aba6a7be0d995a925e72fea36c20d7cbff5
liuyi <www.934392789@qq.com>
2017-11-27 18:04:00 +0800

    增加端口监听

---
+++ personalkeeper/src/test/java/org/liuyi/projects/personalkeeper/test/ProxyTest.java
@@ -0,0 +1,56 @@
+package org.liuyi.projects.personalkeeper.test;
+
+import java.net.*;
+import java.io.*;
+
+class ActionSocket extends Thread {
+	private Socket socket = null;
+
+	public ActionSocket(Socket s) {
+		this.socket = s;
+	}
+
+	public void run() {
+		try {
+			this.action();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void action() throws Exception {
+		if (this.socket == null) {
+			return;
+		}
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+		String line;
+		StringBuilder headStr=new StringBuilder();
+//		 //读取HTTP请求头，并拿到HOST请求头和method
+        while (null != (line = br.readLine())) {
+            System.out.println(line);
+            headStr.append(line + "\r\n");
+            if (line.length() == 0) {
+                break;
+            } else {
+//                String[] temp = line.split(" ");
+//                if (temp[0].contains("Host")) {
+//                    host = temp[1];
+//                }
+            }
+        }
+		System.out.println(headStr.toString());
+		br.close();
+	}
+}
+
+public class ProxyTest {
+	public static void main(String args[]) throws Exception {
+		ServerSocket server = new ServerSocket(8888);
+		while (true) {
+			Socket socket = server.accept();
+			ActionSocket ap = new ActionSocket(socket);
+			ap.start();
+		}
+	}
+}
