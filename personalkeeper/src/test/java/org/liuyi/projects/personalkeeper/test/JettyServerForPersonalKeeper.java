ebf961b370a8ce9f754f9fe704d70bdd11704447
liuyi <www.934392789@qq.com>
2018-01-26 09:17:01 +0800

    去除无用文件

--- personalkeeper/src/test/java/org/liuyi/projects/personalkeeper/test/JettyServerForPersonalKeeper.java
+++ personalkeeper/src/test/java/org/liuyi/projects/personalkeeper/test/JettyServerForPersonalKeeper.java
@@ -9,23 +9,7 @@ import org.junit.Ignore;
 @Ignore
 public class JettyServerForPersonalKeeper {
 	public static void main(String[] args) throws Exception {
-//		// 服务器的监听端口
-//		Server server = new Server(9090);
-//		// 关联一个已经存在的上下文
-//		WebAppContext context = new WebAppContext("src/main/webapp","/personalkeeper");
-//
-//		// 设置描述符位置
-//		context.setDefaultsDescriptor("webdefault.xml");
-//		server.setHandler(context);
-//
-//		try {
-//			server.start();
-//			// server.join();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		System.out.println("server is  start");
 		String webapp = "src/main/webapp";  
-        new JettyServerStart(webapp, 9090, "/personalkeeper").start();  
+        new JettyServerStart(webapp, 8080, "/personalkeeper").start();  
 	}
 }
